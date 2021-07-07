bd4c0a4bd543f7396ecdfe0dde1f2bca1b1a974d
slowrocky <matus.holotnak@gmail.com>
2021-07-08 00:33:20 +0200

    add array of names for squad members and enemies

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
 // mixed messages random generator
+
+// random generated character names of Mass Effect
+const randomSquadMember = () => {
+  const characterNames = [
+    "Commander Shepard",
+    "Jacob Taylor",
+    "Kaidan Alenko",
+    "Ashley Williams",
+    "Garrus Vakarian",
+    "Urdnot Wrex",
+    "Tali'Zorah",
+    "Liara T'Soni",
+    "Miranda Lawson",
+    "EDI",
+    "Mordin Solus",
+    "Jack",
+    "Grunt",
+    "Thane Krios",
+    "Samara",
+    "Morinth",
+    "Legion",
+    "Zaeed Massani",
+    "Kasumi Goto",
+    "James Vega",
+    "Javik",
+  ];
+
+  const randomEnemy = [
+    "The Reapers",
+    "Saren Arterius",
+    "Matriarch Benezia",
+    "Donnel Udina",
+    "The Thorian",
+    "Cerberus",
+    "Kai Leng",
+    "Oleg Petrovsky",
+    "The Collectors",
+    "The Shadow Broker",
+    "Dr. Amanda Kenson",
+    "Maya Brooks",
+    "The Kett",
+  ];
+};
